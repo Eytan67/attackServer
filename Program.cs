1285f919f060c72338778dc865190b14c1769f46
Eytan zichel <eytanzichel@gmail.com>
2024-08-06 11:38:13 +0300

    finish exs

--- Program.cs
+++ Program.cs
@@ -8,8 +8,23 @@ namespace attackServar
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            //await temporary.GetData();
+            //Console.WriteLine("Recive data!");
+            //Console.ReadLine();
+
+            Task<string> taskA = temporary.GetDataFromServisAAsync();
+            Task<string> taskB = temporary.GetDataFromServisBAsync();
+
+            await Task.WhenAll(taskA, taskB);
+            Console.WriteLine(taskA.Result);
+            Console.WriteLine(taskB.Result);
+
+            Console.ReadLine();
+
         }
+
+
     }
 }
---
+++ temporary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace attackServar
+{
+    public static class temporary
+    {
+        public static async Task GetData()
+        {
+            string path = @"C:\Users\user\source\repos\attackServar\TextFile1.txt";
+            string text = "";
+            if (File.Exists(path))
+            {
+                await Task.Run( () => { text = File.ReadAllText(path); });
+
+            }
+            Console.WriteLine(text);
+        }
+        public static async Task<string> GetDataFromServisAAsync()
+        {
+
+            await Task.Delay(1000);
+            return "data from servis a";
+        }
+
+        public static async Task<string> GetDataFromServisBAsync()
+        {
+
+            await Task.Delay(2000);
+            return "data from servis b";
+        }
+    }
+}
